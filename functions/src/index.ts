3e58a11675122a6e53935474357bdf12b828b006
SerTanmay <tanmay36rocks@gmail.com>
2018-09-23 02:27:57 +0530

    video2 full

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,12 +6,12 @@ admin.initializeApp();
  // https://firebase.google.com/docs/functions/typescript
 
  export const getBostonWeather = functions.https.onRequest((request, response) => {
-    const promise = admin.firestore().doc('cities-weather/boston-ma-us').get();
-    const p2 = promise.then(snapshot => {
+    admin.firestore().doc('cities-weather/boston-ma-us').get()
+    .then(snapshot => {
         const data = snapshot.data()
         response.send(data)
     })
-    p2.catch(error=>{
+    .catch(error=>{
         //Handle the error
         console.log(error)
         response.status(500).send(error)
