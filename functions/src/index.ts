4d536bb53b5e85cfc9a73d6ccd1695225b3c885d
SerTanmay <tanmay36rocks@gmail.com>
2018-09-26 00:06:36 +0530

    video4

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,24 +1,56 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+//import { request } from 'http';
 admin.initializeApp();
 
  // Start writing Firebase Functions
  // https://firebase.google.com/docs/functions/typescript
 
-export const onBostonWeatherUpdate = 
-functions.firestore.document("cities-weather/boston-ma-us").onUpdate(change => {
-    const after = change.after.data()
-    const payload = {
-        data:{
-            temp: String(after.temp),
-            conditions: after.conditions
+ //const firestore = new Firestore();
+ //const settings = {/* your settings... */ timestampsInSnapshots: true};
+ //firestore.settings(settings);
+//
+export const getBostonAreaWeather =
+functions.https.onRequest((request, response) => {
+    admin.firestore().doc("areas/greater-boston").get()
+    .then(areaSnapshot => {
+        const cities = areaSnapshot.data().cities
+        const promises = []
+        for (const city in cities){
+            const p = admin.firestore().doc(`cities-weather/${city}`).get()
+            promises.push(p)
         }
-    }
-    return admin.messaging().sendToTopic("weather_boston-ma-us", payload)
-    //.catch(error => {
-    //    console.error("FCM failed", err)
-    //})
+        return Promise.all(promises)
+    })
+    .then(citySnapshots => {
+        const results = []
+        citySnapshots.forEach(citySnap => {
+            const data = citySnap.data()
+            data.city = citySnap.id
+            results.push(data)
+        })
+        response.send(results)
+    })
+    .catch (error => {
+        console.log(error)
+        response.status(500).send(error)
+    })   
 })
+
+//export const onBostonWeatherUpdate = 
+//functions.firestore.document("cities-weather/boston-ma-us").onUpdate(change => {
+//    const after = change.after.data()
+//    const payload = {
+//        data:{
+//            temp: String(after.temp),
+//            conditions: after.conditions
+//        }
+//    }
+//    return admin.messaging().sendToTopic("weather_boston-ma-us", payload)
+//    //.catch(error => {
+//    //    console.error("FCM failed", err)
+//    //})
+//})
 
  export const getBostonWeather = functions.https.onRequest((request, response) => {
     admin.firestore().doc('cities-weather/boston-ma-us').get()
@@ -26,7 +58,7 @@ functions.firestore.document("cities-weather/boston-ma-us").onUpdate(change => {
         const data = snapshot.data()
         response.send(data)
     })
-    .catch(error=>{
+    .catch(error => {
         //Handle the error
         console.log(error)
         response.status(500).send(error)
